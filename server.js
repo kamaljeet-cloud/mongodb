bf1e6726c809eba8c0bb867ada20d5bf5047fd84
kamaljeet-cloud <51871531+kamaljeet-cloud@users.noreply.github.com>
2019-08-01 15:49:00 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(4000).sockets;
+
+//connect to mongodb
+mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
+    if(err){
+        throw err;
+    }
+    console.log('Mongodb connected....');
+
+    //connect to socket.io
+    /*client.on('connection', function(socket){
+        let chat = db.collection('chats');
+
+
+        //create function to send status
+        sendStatus = function(s){
+            socket.emit('status', s);
+
+        } 
+
+        //get chats from mongo collection
+        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
+            if(err){
+                throw err;
+            }
+            //emmit the messages
+            socket.emit('output',res);
+        });
+
+        //handle input  events
+        socket.on('input', function(data){
+            let name = data.name;
+            let message = data.message;
+
+            //check for name and message
+            if(name == ''|| message == ''){
+
+                //send error status
+                sendStatus('Please enter a name and message');
+            }else{
+                //insert message
+                chat.insert({name: name, message: message},function(){
+                    client.emit('output', [data]);
+
+                    //send status object
+                    sendStatus({
+                        message: 'Message sent',
+                        clear: true
+                    });
+                });
+            }
+
+        });
+
+        //handle clear
+        socket.on('clear', function(data){
+            //remove  all chats  from collection
+            chat.remove({}, function(){
+                //emit cleared
+                socket.emit('cleared');
+
+            });
+
+        });
+    });*/
+
+});
